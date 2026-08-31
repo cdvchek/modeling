c4870af85d3c5a63f55b51e9959ee9aa5d1b7f44
Carsdan Dvorachek <cdvchek@gmail.com>
2026-08-31 19:16:39 -0400

    extrusion is unimplemented, started work on the half edge data structure manipulation toolbox

--- src/scene/mesh/mesh_data_cpu.cpp
+++ src/scene/mesh/mesh_data_cpu.cpp
@@ -539,3 +539,48 @@ std::vector<Triangle> MeshData::triangulateFace(FaceHandle handle) const {
     // this should give you what you need to then append the generated triangle indices
     return earclipping(projVerts);
 }
+
+VertexHandle MeshData::addVertex(const Vec3& position) {
+    return m_vertices.insert(Vertex{position});
+}
+
+EdgeHandle MeshData::addEdge(VertexHandle origin, VertexHandle tip) {
+    
+}
+// FaceHandle addFace(const std::vector<VertexHandle>& handles); // Create a face and its half-edge loop from CCW vertices
+// FaceHandle addTriangle(VertexHandle v0, VertexHandle v1, VertexHandle v2); // Convenience wrapper for creating a triangle
+// FaceHandle addQuad(VertexHandle v0, VertexHandle v1, VertexHandle v2, VertexHandle v3); // Convenience wrapper for creating a quad
+// VertexHandle duplicateVertex(VertexHandle handle); // Duplicate a vertex at the same position
+
+// void pairEdges(EdgeHandle a, EdgeHandle b); // Pair two half-edges
+// void unpairEdge(EdgeHandle handle); // Disconnect an edge from its pair
+// void setEdgeTip(EdgeHandle edge, VertexHandle tip); // Change an edge's tip and dirty the affected face
+// void setVertexEdge(VertexHandle vertex, EdgeHandle edge); // Set a representative outgoing half-edge
+// void setFaceEdge(FaceHandle face, EdgeHandle edge); // Set a representative boundary edge for a face
+
+// VertexHandle getEdgeOrigin(EdgeHandle handle) const; // Return the starting vertex of the half edge (not the tip)
+// std::vector<EdgeHandle> getFaceEdges(FaceHandle handle) const; // Return all half-edges around a face in order
+// std::vector<u32> getFaceVertices(u32 face) const; // Return all vertices around a face in order
+// std::vector<EdgeHandle> getVertexEdges(VertexHandle handle) const; // Return all half-edges (incoming and outgoing) incident to a vertex
+// std::vector<EdgeHandle> getVertexIncomingEdges(VertexHandle handle) const; // Return all half-edges whose tip is the vertex
+// std::vector<EdgeHandle> getVertexOutgoingEdges(VertexHandle handle) const; // Return all half-edges originating at the vertex
+// std::vector<FaceHandle> getVertexFaces(VertexHandle handle) const; // Return all faces touching a vertex
+// std::vector<FaceHandle> getAdjacentFaces(FaceHandle handle) const; // Return all faces sharing an edge with a face
+// std::vector<VertexHandle> getAdjacentVertices(VertexHandle handle) const; // Return all vertices directly connected to a vertex
+    
+// bool isBoundaryEdge(EdgeHandle handle) const; // Return true for any half-edge that is adjacent to only one face
+// bool isBoundaryVertex(VertexHandle handle) const; // Return true for any vertex that has an incident boundary edge
+// bool areVerticesConnected(VertexHandle a, VertexHandle b) const; // Return true if given vertices are adjacent
+// bool areFacesAdjacent(FaceHandle a, FaceHandle b) const; // Return true if given faces are adjacent
+// EdgeHandle findEdge(VertexHandle origin, VertexHandle tip) const; // Finds a directed half-edge between two vertices, returns invalid index if no half-edge found
+
+// VertexHandle splitEdge(EdgeHandle handle, const Vec3& position); // Insert a vertex into an existing edge
+// VertexHandle splitEdge(EdgeHandle handle, f32 t); // Insert a vertex somewhere along an edge
+// void collapseEdge(EdgeHandle handle); // Collapse an edge and merge its vertices
+// FaceHandle splitFace(FaceHandle face, VertexHandle vertexA, VertexHandle vertexB); // Connect two vertices of a face, splitting it into two faces
+// void mergeFaces(FaceHandle handle); // Remove the shared edge between two faces and combine them
+// void detachVertexFromFace(VertexHandle vertex, FaceHandle face); // Duplicate/separate a vertex so the face can move independently
+// void detachFace(FaceHandle handle); // Duplicate the necessary boundary topology so the face is disconnected from its neighbors
+// void removeFace(FaceHandle handle); // Remove a face while leaving appropriate boundary topology
+// void removeEdge(EdgeHandle handle); // Remove an edge/paair while repairing references
+// void removeVertex(VertexHandle handle); // Remove an unused vertex
